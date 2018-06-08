5ca885cbc300d092536114bc7546b7ab096956e8
Jim Vargas <jimball1@hotmail.com>
2018-06-08 16:42:51 -0700

    interesting: zeroes inbetween every node

--- cs162_lab10.cpp
+++ cs162_lab10.cpp
@@ -8,9 +8,22 @@ int main()
     display_all(head);
 
     //PLEASE PUT YOUR CODE HERE to call the function assigned
+    int firstNlast = display_first_and_last(head);
+    bool didItWork = insert_before_last(head, 100000);
+    if (didItWork)
+        cout << "\nIt worked\n";
+    else
+        cout << "\nNaw\n";
+
+    cout << "\nKilled this many off the top: " << remove_first_two(head) << endl;
 
 
+    node * newHead = new node;
+    copy_all(newHead, head);
+    
     display_all(head);
+    cout << "the copy:";
+    display_all(newHead);
     
     return 0;
 }
--- cs162_list.cpp
+++ cs162_list.cpp
@@ -1,5 +1,7 @@
 #include "cs162_list.h"
+#include <iostream>
 
+using namespace std;
 
 //These are the functions to implement for this lab
 //Like Lab9, we are not using clases here
@@ -14,27 +16,116 @@
 //Step 2 - Practice Traversal
 int display_first_and_last(node * head)
 {
+    int pile = 0;
+    if (head != NULL)
+    {
+        node * current = head;
+        pile += current->data;
 
-           //Write your code here
+        cout << "\nFirst node: " << current->data << endl;
+
+        while (current->next != NULL)
+            current = current->next;
+
+        pile +=current->data;
+
+        cout << "\nLast node: " << current->data << endl;
+        cout << "Sum between the two: " << pile << endl;
+
+    }       //Write your code here
+    else
+        cout << "\nNo nodes\n";
+
+    return pile;
 }
 
 //Step 3 - Practice Insertion
 bool insert_before_last(node * & head, int to_add)
 {
-           //Write our code here
+    //Write our code here
+    //
+    if (head!=NULL)
+    {
+        node * previous = NULL;
+        node * current = head;
+
+        while (current->next !=NULL)
+        {
+            previous = current;
+            current = current->next;
+        }
+
+        node * temp = new node;
+        temp->data = to_add;
+        previous->next = temp;
+        temp->next = current;
+
+        return true;
+    } 
+    return false;
 }
 
 //Step 4 - Practice Removal
 int remove_first_two(node * & head)
 {
-          //Write your code here
+    int victims = 0;
+    if (head != NULL)
+    {
+        node * firsty = head;
+        if (firsty->next !=NULL)
+        {
+            node * secondy = head->next;
+            head = secondy->next;
+            //destroy(secondy);
+            //destroy(firsty);
+            victims++;
+        }
+
+        //destroy(firsty);
+        victims++;
+    }
+    return victims;
 }
 
 
 //Challenge - Make a complete copy  of the entire LLL creating
 //a completely new LLL
+//the 'source' the givenn node to be copied
+//the 'destination' is the node that will be new, a copy of the given source
 void copy_all(node * & destination_head, node * source_head)
 {
-          //Write your code here
+    if ( destination_head==NULL || source_head == NULL)
+        return;
 
+    node * nextStepperBoiThing = new node;
+    nextStepperBoiThing->data = source_head->data;
+    destination_head->next = nextStepperBoiThing;
+
+    node * evenNexter = new node;
+    nextStepperBoiThing->next = evenNexter;
+    copy_all(evenNexter, source_head->next);
+
+    return;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
